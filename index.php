bf4ae3cffde22c277225a6fd1c9700e05e6444fb
Andriy Lozinskyi <loz.andriy@gmail.com>
2018-09-07 10:55:02 +0300

    create README file

--- index.php
+++ index.php
@@ -7,25 +7,6 @@ use Infrastructure\Request\Handler\Exception\NotFoundHandlerException;
 
 require_once 'autoload.php';
 
-/*
-ОПИС ПРЕДМЕТНОЇ ОБЛАСТІ:
-
-Web-aплікашка роздруковує меню для різних кухонь.
-
-Guisine - кухня
-Meal - їжа, MealType - час прийому їжі (сніданок, обід, вечеря)
-Dish - страва
-Ingredient - інгредієнти для страви
-
-Cuisine містить Meals (Set із типів Їжі),
-Meal в свою чергу міcтить Dishes (Set із страв),
-Dish містить Ingredients (Set з інгредієнтів)
-
-http://localhost:8000/?print=georgian - роздрукувати грузинську кухню
-http://localhost:8000/?print=ukrainian - роздрукувати українську кухню
-
-*/
-
 try {
     $httpRequest = new HttpRequest();
 
